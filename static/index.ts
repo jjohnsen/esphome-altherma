d315b0a72a1459b5f41167c1b592d0460d20df53
Jan Age Johnsen <jan.age.johnsen@gmail.com>
2026-04-10 19:32:20 +0000

    Fix redirects file and test worker

---
+++ static/index.ts
@@ -0,0 +1,11 @@
+export interface Env {
+  ASSETS: Fetcher;
+}
+
+export default {
+  async fetch(request: Request, env: Env): Promise<Response> {
+    const url = new URL(request.url);
+
+    return env.ASSETS.fetch(request);
+  }
+}
